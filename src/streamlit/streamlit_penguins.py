eefda2d58a5cb2592554da27d88926e58f24b37e
Silvia Conde <silvia.condearincon@gmail.com>
2021-08-04 20:24:44 +0200

    API day 1

--- src/streamlit/streamlit_penguins.py
+++ src/streamlit/streamlit_penguins.py
@@ -1,5 +1,32 @@
 import streamlit as st
+import altair as alt
+import os
 
 st.title("Palmer penguins")
 
-id_number = st.text_input("Which penguin am I?")
+images = st.image([os.path.join(os.path.dirname(__file__),
+                   "../../assets/Adelie_penguin.jpg"),
+                   os.path.join(os.path.dirname(__file__),
+                                "../../assets/Chinstrap_penguin.jpg"),
+                   os.path.join(os.path.dirname(__file__), '../../assets/Gentoo_penguin.jpg')], caption=["Adelie", "Chinstrap", "Gentoo"])
+
+
+# Elegir pingüino por ID
+
+id_number = st.text_input("Which penguin am I? (ID_number)")
+
+# Escoger una especie
+
+species = st.radio("Pick a specie", ("Adelie", "Chinstrap", "Gentoo"))
+
+
+# Comparar medidas entre especies o machos y hembras
+
+# source = data.penguins()
+
+# alt.Chart(source).mark_circle(size=60).encode(
+# x='',
+# y='',
+#  color='',
+#   tooltip=[']
+# ).interactive()
