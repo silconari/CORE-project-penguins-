cb961331d18c9384d16ffa653e4770e221eabe85
Silvia Conde <silvia.condearincon@gmail.com>
2021-08-14 14:05:45 +0200

    adjusting maps

--- src/streamlit_dashboard/islands.py
+++ src/streamlit_dashboard/islands.py
@@ -2,6 +2,8 @@ import streamlit as st
 import os
 import requests
 import pandas as pd
+from streamlit_folium import folium_static
+import folium
 
 
 def render_streamlit():
@@ -26,7 +28,53 @@ def render_streamlit():
 
                        ], width=300, caption=["Emperor", "King", "Macaroni", "rockhopper", "Royal", "rare"])
 
-    st.title("Which is nearest island from my ubication?")
+    # Geolocalización de las islas del Archipiélago Palmer
+
+    st.header("Where penguins live?")
+
+    initial_location = []
+    folium_markers = []
+
+    def icon(): return folium.features.CustomIcon(
+        'https://d29fhpw069ctt2.cloudfront.net/icon/image/49037/preview.svg', icon_size=(30, 30))
+
+    Torgersen = requests.get(
+        "http://127.0.0.1:5000/location?name=Torgersen%20Island,%20Antarctica").json()
+
+    initial_location = [-64.968089, -63.551734]
+
+    folium_markers.append(
+        folium.Marker(
+            [Torgersen["latitude"], Torgersen["longitude"]], popup="Torgersen Island", tooltip="Torgersen Island", icon=icon()
+        )
+
+    )
+
+    Biscoe = requests.get(
+        "http://127.0.0.1:5000/location?name=Biscoe%20Islands,%20Antarctica").json()
+
+    folium_markers.append(
+        folium.Marker(
+            [Biscoe["latitude"], Biscoe["longitude"]], popup="Biscoe Island", tooltip="Biscoe Island", icon=icon()
+        )
+    )
+
+    # En el caso de la isla Dream, la API a la que hago la petición no es capaz de ubicarla correctamente,
+    # fuerzo a folium a utilizar las coordenadas reales
+
+    folium_markers.append(
+        folium.Marker(
+            [-64.7333323, -64.2420877], popup="Dream Island", tooltip="Dream Island", icon=icon()
+        )
+
+    )
+    m = folium.Map(location=initial_location, zoom_start=7)
+    for elem in folium_markers:
+        elem.add_to(m)
+
+    folium_static(m)
+
+    st.header("Which is nearest island from my ubication?")
 
     form = st.form(key='my-form')
     coordinates = form.text_input('Enter latitude,longitude')
